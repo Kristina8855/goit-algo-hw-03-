3e67465aeeacfffe563bbd316eff74e7c77a196c
Kristina8855 <163170933+Kristina8855@users.noreply.github.com>
2024-04-19 17:18:35 +0200

    2

--- Homework.py
+++ Homework.py
@@ -34,10 +34,41 @@ def get_numbers_ticket(min, max, quantity):
 
 
 min = int(input('input min numbers 1-999:'))
-max = int(input('inputmax numbers 2-1000:'))
+max = int(input('input max numbers 2-1000:'))
 quantity = int(input('input quantity:'))
 
 # results
 
 lottery_numbers = get_numbers_ticket(min, max, quantity)
-print("yours lotery numbers:", lottery_numbers)
+print("yours lottery numbers:", lottery_numbers)
+
+
+
+# Третє завдання
+import re
+
+def normalize_phone(phone_numbers):
+    sanitized_numbers = []
+    for phone in phone_numbers:
+        cleaned_number = re.sub(r'[^0-9]', '', phone)
+        if not cleaned_number.startswith('+'):
+            if cleaned_number.startswith('380'):
+                cleaned_number = '+' + cleaned_number
+            else:
+                cleaned_number = '+38' + cleaned_number
+        sanitized_numbers.append(cleaned_number)
+    return sanitized_numbers
+
+phone_numbers = [
+    "+38(050)123-32-34",
+    "0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11"
+]
+
+sanitized_numbers = normalize_phone(phone_numbers)
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
+
+
